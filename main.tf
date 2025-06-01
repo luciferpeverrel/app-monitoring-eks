bccb73f21e7700d0c1d444f798ef663a4e1697fb
Abhigyan Sanyal <abhigyansanyal@Abhigyans-MacBook-Air.local>
2025-06-01 05:36:49 +0530

    version changes

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ module "vpc" {
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = var.cluster_name
-  cluster_version = "1.29"
+  cluster_version = "1.31"
   subnets         = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
